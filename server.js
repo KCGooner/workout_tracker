f241f88141b37bca5495cc6ca1d852c5023865a8
Joe Huelsman <kcgooner91@gmail.com>
2020-04-11 12:00:05 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
 
 //Routes
-app.use(require("./routes/html-routes"));
-app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 //Validation server is running on correct port
 app.listen(PORT, () => {
